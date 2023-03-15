dd6e479fd824e55b0822cc022704d8dc6b272f85
Marcus Hopp <marcushopp@MBPvonMarcus2.fritz.box>
2023-03-15 19:53:53 +0100

    Initial commit

---
+++ workshop_ecole42/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workshop_ecole42
+//
+//  Created by Marcus Hopp on 15.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
